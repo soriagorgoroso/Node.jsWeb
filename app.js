4ebf31290daddc726137cc2ba1c4b1187b9b8a58
soriasebastian <soriagorgoroso@gmail.com>
2022-06-21 13:31:21 -0300

    .env

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-const express = require('express')
-const hbs = require('hbs')
+require("dotenv").config();
+const express = require("express");
+const hbs = require("hbs");
 
+const app = express();
+const port = proces.ev.PORT;
 
-const app = express()
-const port = 8080
-
-app.set('view engine', 'hbs')
-hbs.registerPartials(__dirname + "/views/partials")
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 //Servir contenido estatico
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req,res)=> {
-    res.render('home',{
-        nombre: 'Sebastian Soria',
-        titulo: 'Proyecto Node.Js whit HBS'
-    });
-})
-app.get('/generic', (req,res)=> {
-    res.render('generic',{
-        nombre: 'Sebastian Soria',
-        titulo: 'Proyecto Node.Js whit HBS'
-    });
-})
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "Sebastian Soria",
+    titulo: "Proyecto Node.Js whit HBS",
+  });
+});
+app.get("/generic", (req, res) => {
+  res.render("generic", {
+    nombre: "Sebastian Soria",
+    titulo: "Proyecto Node.Js whit HBS",
+  });
+});
 
-app.get('/elements', (req,res)=> {
-    res.render('elements',{
-        nombre: 'Sebastian Soria',
-        titulo: 'Proyecto Node.Js whit HBS'
-    });
-})
+app.get("/elements", (req, res) => {
+  res.render("elements", {
+    nombre: "Sebastian Soria",
+    titulo: "Proyecto Node.Js whit HBS",
+  });
+});
 
-app.get('*', (req,res)=> {
-    res.sendFile(__dirname + '/views/public')
-})
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/views/public");
+});
 
-
-app.listen(port, ()=> {
-    console.log(`El SERVIDOR esta corriendo en http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`El SERVIDOR esta corriendo en http://localhost:${port}`);
+});
