cd5ea29ebd6752a9b83ee9795e105ee440efe3c8
Sebastian Soria <soriagorgoroso@gmail.com>
2022-06-23 12:32:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const hbs = require("hbs");
 
 const app = express();
-const port = process.ev.PORT;
+const port = process.env.PORT;
 
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
